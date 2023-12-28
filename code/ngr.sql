b2d52167d0b8883ccd7c722577399a910b0f4119
Małgorzata Wierzba <gosia.wierzba@gmail.com>
2023-12-28 21:17:56 +0100

    Add SQL data export script * Complete list of valid subject codes * Demographic data

---
+++ code/ngr.sql
@@ -0,0 +1,23 @@
+-- psql -U grieg --file ./code/ngr.sql
+
+\f '|'
+\pset footer
+
+CREATE TEMP VIEW ngr AS
+  SELECT 
+    s.sid, s.code, s.stid,
+    q.qid, q.name
+  FROM subject s
+  JOIN qcopy q ON q.rid = s.sid
+  WHERE s.stid = 27
+   AND q.name = 'demo-3-pl' AND q.is_complete;
+
+\o ./data/questionnaires/demo-by-subject.csv
+
+SELECT sid, code,
+       c.opt AS sex,
+       2023 - t.val :: INTEGER AS age
+FROM ngr n
+LEFT JOIN choice c ON c.qid = n.qid AND c.ord = 0
+LEFT JOIN txt t ON t.qid = n.qid AND t.ord = 2
+ORDER BY sid;
