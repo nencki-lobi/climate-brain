cd0b22b716be2a5bf397011858467fc9d60e0d10
Małgorzata Wierzba <gosia.wierzba@gmail.com>
2024-01-07 21:16:13 +0100

    Borrow BIDS-Matlab's code for generating SPM-compatible confounds

---
+++ code/preproc/bids-matlab/run_extract_confounds_fmriprep.m
@@ -0,0 +1,6 @@
+
+datadir = fullfile(pwd, 'ds-ngr/bids/derivatives'); % fmriprep dataset location
+resdir = fullfile(pwd, 'ds-ngr/bids/results'); % output location
+
+extract_confounds_fmriprep(datadir, resdir, 'stories')
+extract_confounds_fmriprep(datadir, resdir, 'cet')
