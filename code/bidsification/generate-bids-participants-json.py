55086b33078de74c625bead9e047c679f6c072c2
Małgorzata Wierzba <gosia.wierzba@gmail.com>
2024-07-30 20:17:54 +0200

    Add code to generate BIDS participans TSV and JSON files

---
+++ code/bidsification/generate-bids-participants-json.py
@@ -0,0 +1,94 @@
+# Import dependencies
+
+import json
+import os
+
+# Define functions
+
+def add_entry(data, key, description, levels_or_units):
+    if isinstance(levels_or_units, dict):
+        data[key] = {"Description": description, "Levels": levels_or_units}
+    else:
+        data[key] = {"Description": description, "Units": levels_or_units}
+
+
+# Prepare entries
+
+entries = [
+    ("condition", "Experimental condition to which the participant belonged",
+     {"ANG": "Anger",
+      "HOP": "Hope",
+      "NEU": "Neutral"}),
+    ("sex", "Sex", {"0": "Female", "1": "Male"}),
+    ("age", "Age", "Years"),
+    ("residence", "Place of residence",
+     {"0": "Big city",
+      "1": "Suburbs of a big city",
+      "2": "Small city",
+      "3": "Country village",
+      "4": "A farm or home in the countryside"}),
+    ("education", "Education level",
+     {"0": "Incomplete primary",
+      "1": "Primary",
+      "2": "Lower secondary",
+      "3": "Vocational",
+      "4": "Upper secondary",
+      "5": "Bachelor's degree or engineering degree",
+      "6": "Master's degree or medical degree",
+      "7": "Doctorate (Ph.D.), habilitation, or professorship",
+      "8": "Other"}),
+    ("CCC", "How concerned are you about climate change?",
+     {"0": "Not at all concerned",
+      "1": "Not very concerned",
+      "2": "Somewhat concerned",
+      "3": "Very concerned",
+      "4": "Extremely concerned"}),
+    ("actions", "What actions do you consider most important in addressing climate change?",
+     "Points on a scale, where: 1 - Collective actions (by states, institutions, corporations), 5 - Individual actions (by ordinary people)"),
+    ("lifestyle", "Compared to other people, do you consider your lifestyle ...?",
+     "Points on a scale, where: 1 - Definitely climate-unfriendly, 5 - Definitely climate-friendly"),
+    ("driver", "Do you have a driver's license?", {"0": "Yes", "1": "No"}),
+    ("car", "How often do you use a car (e.g. as a driver or a passenger)?",
+     {"0": "Once a year or several times a year",
+      "1": "Once a month or several times a month",
+      "2": "Once a week or several times a week",
+      "3": "Every day"}),
+    ("politics1", "Do you have any political views?", {"0": "No", "1": "Yes"}),
+    ("politics2",
+     "In politics, the terms /left/ and /right/ are used. Can you describe your political views using these terms?",
+     {"0": "No", "1": "Yes"}),
+    ("politics3", "Please indicate your political views.",
+     "Points on a scale, where: 0 - Left, 10 - Right"),
+    ("SES", "Which of the descriptions below comes closest to how you feel about your household income nowadays?",
+     {"0": "Living comfortably on present income",
+      "1": "Coping on present income",
+      "2": "Finding it difficult on present income",
+      "3": "Finding it very difficult on present income"}),
+    ("PCAE_i", "Personal and Collective Action Efficacy (Chu & Yang, 2020) - Individual efficacy", "Total score"),
+    ("PCAE_c", "Personal and Collective Action Efficacy (Chu & Yang, 2020) - Collective efficacy", "Total score"),
+    ("PCAE", "Personal and Collective Action Efficacy (Chu & Yang, 2020)", "Total score"),
+    ("PD", "Psychological Distance to Climate Change (Valkengoed, Steg & Perlaviciute, 2021)", "Total score"),
+    ("NR", "Nature Relatedness (Nisbet & Zelenski, 2013)", "Total score"),
+    ("ICE_anger", "Inventory of Climate Emotions (Marczak et al. 2023) - Climate anger", "Total score"),
+    ("ICE_discontent", "Inventory of Climate Emotions (Marczak et al. 2023) - Climate discontent", "Total score"),
+    ("ICE_enthusiasm", "Inventory of Climate Emotions (Marczak et al. 2023) - Climate enthusiasm", "Total score"),
+    ("ICE_powerlessness", "Inventory of Climate Emotions (Marczak et al. 2023) - Climate powerlessness", "Total score"),
+    ("ICE_guilt", "Inventory of Climate Emotions (Marczak et al. 2023) - Climate guilt", "Total score"),
+    ("ICE_isolation", "Inventory of Climate Emotions (Marczak et al. 2023) - Climate isolation", "Total score"),
+    ("ICE_anxiety", "Inventory of Climate Emotions (Marczak et al. 2023) - Climate anxiety", "Total score"),
+    ("ICE_sorrow", "Inventory of Climate Emotions (Marczak et al. 2023) - Climate sorrow", "Total score")
+]
+
+# Loop through the entries to generate the final output
+
+final = {}
+
+for key, description, levels_or_units in entries:
+    add_entry(final, key, description, levels_or_units)
+
+# Save output as a nicely formatted JSON file
+
+os.makedirs('../../output/bids', exist_ok=True)
+
+with open('../../output/bids/participants.json', "w") as file:
+    json.dump(final, file, indent=4)
---
+++ code/bidsification/generate-bids-participants.py
@@ -0,0 +1,73 @@
+# Import dependencies
+
+import pandas as pd
+import os
+
+# Load subjects
+
+subjects = pd.read_csv('../../output/condition-by-subject.csv')
+
+# Load demographic data
+
+demo = pd.read_csv('../../data/questionnaires/demographic.csv',
+                   dtype={'val': pd.Int64Dtype()})  # nullable integer
+demo['code'] = demo['code'].str.replace('ngr', '')
+demo.drop('sid', axis=1, inplace=True)
+
+demo_dict = {0: 'sex', 2: 'age', 3: 'residence', 4: 'education',
+             6: 'CCC', 7: 'actions', 8: 'lifestyle', 9: 'driver', 10: 'car',
+             11: 'politics1', 12: 'politics2', 13: 'politics3', 14: 'SES'}
+
+demo_transposed = demo.pivot(
+    index='code',
+    columns=['ord'],
+    values='val'
+).reset_index().rename(columns=demo_dict)
+
+demo_transposed['age'] = 2023 - demo_transposed['age']
+
+# Load questionnaire data
+
+questionnaires = pd.read_csv('../../data/questionnaires/psychometric.csv')
+questionnaires['code'] = questionnaires['code'].str.replace('ngr', '')
+questionnaires.drop('sid', axis=1, inplace=True)
+
+questionnaires_transposed = questionnaires.pivot(
+    index='code',
+    columns=['name', 'ord'],
+    values='opt'
+).reset_index()
+
+# Caluculate questionnaire scores
+
+questionnaires_transposed['PCAE_i'] = questionnaires_transposed[[('PCAE-pl', i) for i in [0, 1]]].sum(axis=1)
+questionnaires_transposed['PCAE_c'] = questionnaires_transposed[[('PCAE-pl', i) for i in [2, 3]]].sum(axis=1)
+questionnaires_transposed['PCAE'] = questionnaires_transposed['PCAE_i'] + questionnaires_transposed['PCAE_c']
+questionnaires_transposed['PD'] = questionnaires_transposed[[('PD-pl', i) for i in [0, 1]]].sum(axis=1)
+questionnaires_transposed['NR'] = questionnaires_transposed[[('NRm-pl', i) for i in [0, 1, 2, 3, 4, 5]]].sum(axis=1)
+questionnaires_transposed['ICE_anger'] = questionnaires_transposed[[('ICE-32-pl', i) for i in [0, 1, 2, 3]]].sum(axis=1)
+questionnaires_transposed['ICE_discontent'] = questionnaires_transposed[[('ICE-32-pl', i) for i in [4, 5, 6, 7]]].sum(axis=1)
+questionnaires_transposed['ICE_enthusiasm'] = questionnaires_transposed[[('ICE-32-pl', i) for i in [8, 9, 11, 12]]].sum(axis=1)
+questionnaires_transposed['ICE_powerlessness'] = questionnaires_transposed[[('ICE-32-pl', i) for i in [13, 14, 15, 16]]].sum(axis=1)
+questionnaires_transposed['ICE_guilt'] = questionnaires_transposed[[('ICE-32-pl', i) for i in [17, 19, 20, 21]]].sum(axis=1)
+questionnaires_transposed['ICE_isolation'] = questionnaires_transposed[[('ICE-32-pl', i) for i in [22, 24, 25, 26]]].sum(axis=1)
+questionnaires_transposed['ICE_anxiety'] = questionnaires_transposed[[('ICE-32-pl', i) for i in [27, 28, 29, 30]]].sum(axis=1)
+questionnaires_transposed['ICE_sorrow'] = questionnaires_transposed[[('ICE-32-pl', i) for i in [31, 32, 33, 34]]].sum(axis=1)
+
+scores = questionnaires_transposed[['code', 'PCAE_i', 'PCAE_c', 'PCAE', 'PD', 'NR',
+                                    'ICE_anger', 'ICE_discontent', 'ICE_enthusiasm', 'ICE_powerlessness',
+                                    'ICE_guilt', 'ICE_isolation', 'ICE_anxiety', 'ICE_sorrow']]
+
+scores.columns = [f'{col[0]}' for col in scores.columns]
+
+# Prepare & save final output
+
+dfs = [subjects, demo_transposed, scores]
+dfs = [df.set_index('code') for df in dfs]
+final = pd.concat(dfs, join='outer', axis=1)
+final = final.add_prefix('sub-', axis=0)
+final = final.reset_index(names=['participant_id'])
+
+os.makedirs('../../output/bids', exist_ok=True)
+
+final.to_csv('../../output/bids/participants.tsv', sep='\t', index=False)
