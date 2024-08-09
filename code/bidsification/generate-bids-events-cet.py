ac676aa04c01bebb09b3533c7fec9e5da9b9ae29
Małgorzata Wierzba <gosia.wierzba@gmail.com>
2024-08-09 21:18:19 +0200

    Add code to generate BIDS events TSV files

---
+++ code/bidsification/generate-bids-events-cet.py
@@ -0,0 +1,99 @@
+# Import dependencies
+import os
+import glob as gl
+import pandas as pd
+import numpy as np
+import re
+
+
+def log2df(logfile, customfile):
+
+    fname = os.path.basename(logfile)
+    prefix = fname.split('-')[0]
+
+    # Read logfile
+    ldata = pd.read_csv(logfile,
+                        sep='\t',
+                        header=None, skiprows=[0, 1, 2, 3],
+                        usecols=[0, 2, 3, 4], names=['participant_id', 'event', 'trial_code', 'time'],
+                        skipfooter=16, engine='python')
+
+    # Extra cleaning steps to handle special cases (fail messages, quit events)
+    ldata = (ldata
+             .query('participant_id == @prefix')
+             .dropna()
+             )
+
+    # Read customfile
+    cdata = pd.read_csv(customfile, sep='\t', header=None, names=['trial_code', 'response'])
+
+    # Merge data from both sources
+    data = pd.merge(ldata, cdata, on="trial_code", how="left")
+
+    # Assign type
+    data['trial_type'] = np.select(
+        [data['trial_code'].str.match("^[mc]"),
+         data['trial_code'].str.match("^d")],
+        ["CET",
+         "dummy"],
+        default=np.nan)
+
+    data.insert(1, 'trial_type', data.pop('trial_type'))
+
+    # Set onset
+    first_pulse = data.query('event == "Pulse"').iloc[0]
+    data['onset'] = (data['time'] - first_pulse['time']) / 10000
+
+    # Set duration
+    data = data.query('event != "Pulse"').reset_index(drop=True)
+    data['duration'] = 10
+    data['RT'] = np.select(
+        [data['response'].isin(['LEFT', 'RIGHT']),
+         data['response'] == 'NONE'],
+        [-data['onset'].diff(periods=-1),
+         10],
+        default=np.nan)
+
+    # Retrive money & carbon values
+    data["money"] = data["trial_code"].apply(lambda x: trial2money(x))
+    data["carbon"] = data["trial_code"].apply(lambda x: trial2carbon(x))
+
+    # Prepare output
+    out = (data
+           .query('trial_code.str.match("^[mcd]")')
+           .drop(columns=['event', 'time'])
+           )
+
+    return out
+
+
+def trial2money(code):
+    pat = re.findall(r'm\d|dl|dr', code)
+    c2m = dict(dl=0, dr=0, m0=0, m1=10, m2=50, m3=80, m4=100, m5=120)
+    out = c2m[pat[0]] if pat else -1
+    return out
+
+
+def trial2carbon(code):
+    pat = re.findall(r'c\d|dl|dr', code)
+    c2c = dict(dl=0, dr=0, c0=0, c1=2, c2=10, c3=25, c4=40, c5=50)
+    out = c2c[pat[0]] if pat else -1
+    return out
+
+
+ngr = pd.read_csv('../../data/questionnaires/subjects.csv')
+subjects = ngr["code"].str.replace('ngr', '')
+
+os.makedirs('../../output/bids/cet-events', exist_ok=True)
+
+dfl = []
+for i, sub in enumerate(subjects):
+    log = gl.glob('../../data/logs/' + sub + '*cet*.log')[0]
+    custom = gl.glob('../../data/logs/' + sub + '*cet*custom.txt')[0]
+    df = log2df(log, custom)
+    dfl.append(df)
+    (df.drop(columns=['participant_id'])
+     .to_csv('../../output/bids/cet-events/sub-' + sub + '_task-cet_events.tsv', sep='\t', index=False))
+final = pd.concat(dfl)
+
+final.to_csv('../../output/bids/cet-events.tsv', sep='\t', index=False)
---
+++ code/bidsification/generate-bids-events-stories.py
@@ -0,0 +1,117 @@
+# Import dependencies
+import os
+import glob as gl
+import pandas as pd
+import numpy as np
+
+
+def log2df(logfile):
+
+    fname = os.path.basename(logfile)
+    prefix = fname.split('-')[0]
+
+    # Read logfile
+    data = pd.read_csv(logfile,
+                       sep='\t',
+                       header=None, skiprows=[0, 1, 2, 3],
+                       usecols=[0, 2, 3, 4], names=['participant_id', 'event', 'trial_code', 'time'],
+                       skipfooter=16, engine='python')
+
+    # Extra cleaning steps to handle special cases (fail messages, quit events)
+    data = (data
+            .query('participant_id == @prefix')
+            .dropna()
+            )
+
+    # Find events
+    isstory = data['trial_code'].str.contains("ANG|HOP|NEU")
+    isquestion = data['trial_code'] == "Valence"
+    isval = data['trial_code'].str.contains("Valence Response")
+    isaro = data['trial_code'].str.contains("Arousal Response")
+
+    # Assign code
+    trial_codes = data[isstory]['trial_code'].values
+    data.loc[isquestion, 'trial_code'] = 'Q_' + trial_codes
+
+    # Assign type
+    data['trial_type'] = np.select(
+        [isstory, isquestion],
+        [data["trial_code"].apply(lambda x: code2type(x)), "Q"],
+        default=np.nan)
+
+    data.insert(1, 'trial_type', data.pop('trial_type'))
+
+    # Set onset
+    first_pulse = data.query('event == "Pulse"').iloc[0]
+    data['onset'] = (data['time'] - first_pulse['time'])/10000
+
+    # Set duration
+    data['duration'] = np.select(
+        [isstory, isquestion],
+        [15, 11])
+
+    # Retrieve valence & arousal ratings
+    valence = (data[isval]['trial_code']
+               .squeeze()
+               .str.split(': ').str[1]
+               .values)
+
+    arousal = (data[isaro]['trial_code']
+               .squeeze()
+               .str.split(': ').str[1]
+               .values)
+
+    data.loc[isstory, 'valence'] = valence
+    data.loc[isquestion, 'valence'] = valence
+
+    data.loc[isstory, 'arousal'] = arousal
+    data.loc[isquestion, 'arousal'] = arousal
+
+    # Prepare output
+    out = (data[isstory | isquestion]
+           .drop(columns=['event', 'time'])
+           .sort_index())
+
+    return out
+
+
+def code2type(trial_code):
+    anger = ['ANG8', 'ANG20', 'ANG17', 'ANG30', 'ANG25', 'ANG6', 'ANG9', 'ANG1', 'ANG12', 'ANG3', 'ANG21', 'ANG7']
+    hope = ['HOP23', 'HOP15', 'HOP27', 'HOP13', 'HOP17', 'HOP26', 'HOP5', 'HOP11', 'HOP19', 'HOP7', 'HOP22', 'HOP16']
+    neutral = ['NEU14', 'NEU30', 'NEU9', 'NEU15', 'NEU1', 'NEU21', 'NEU29', 'NEU23', 'NEU6', 'NEU12', 'NEU24', 'NEU11']
+    control = ['NEU26', 'NEU16', 'NEU4', 'NEU25', 'NEU3', 'NEU18', 'NEU10', 'NEU22', 'NEU17', 'NEU27', 'NEU2', 'NEU5']
+
+    trial_type = ''
+
+    if trial_code in anger:
+        trial_type = 'ANG'
+    elif trial_code in hope:
+        trial_type = 'HOP'
+    elif trial_code in neutral:
+        trial_type = 'NEU'
+    elif trial_code in control:
+        trial_type = 'CON'
+    return trial_type
+
+
+ngr = pd.read_csv('../../data/questionnaires/subjects.csv')
+subjects = ngr["code"].str.replace('ngr', '')
+
+runs = ['R1', 'R2', 'R3']
+
+os.makedirs('../../output/bids/stories-events', exist_ok=True)
+
+dfl = []
+for i, sub in enumerate(subjects):
+    for j, r in enumerate(runs):
+        run = r
+        log = gl.glob('../../data/logs/' + sub + '*stories*' + run + '*.log')[0]
+        df = log2df(log)
+        df.insert(1, "run", run)
+        dfl.append(df)
+        (df.drop(columns=['participant_id', 'run'])
+         .to_csv('../../output/bids/stories-events/sub-' + sub + '_task-stories_run-0' + str(j+1) + '_events.tsv',
+                 sep='\t', index=False))
+final = pd.concat(dfl)
+
+final.to_csv('../../output/bids/stories-events.tsv', sep='\t', index=False)
