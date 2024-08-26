b8612816df1d8e2422a41fdbcd90585eb56add9a
Małgorzata Wierzba <gosia.wierzba@gmail.com>
2024-08-26 16:19:53 +0200

    Use different names in when modelling stories task

--- code/log-processing/multiple_conditions_stories.m
+++ code/log-processing/multiple_conditions_stories.m
@@ -2,14 +2,14 @@ function multiple_conditions_stories(events, run)
     
     %% Define events
 
-    SEMO = events(matches(events.trial_type,["ANG","HOP","NEU"]), :);
-    SCON = events(matches(events.trial_type,"CON"), :);
+    target = events(matches(events.trial_type,["ANG","HOP","NEU"]), :);
+    control = events(matches(events.trial_type,"CON"), :);
     questions = events(matches(events.trial_type,"Q"), :);
     
     %% Create multiple conditions structure
 
-    names = {'SEMO', 'SCON', 'Q'};
-    onsets = {SEMO.onset, SCON.onset, questions.onset};
+    names = {'TAR', 'CON', 'Q'};
+    onsets = {target.onset, control.onset, questions.onset};
     durations = {15, 15, 11};
     
     %% Save output
